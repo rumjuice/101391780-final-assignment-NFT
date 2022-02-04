0b0bee9caa2d329f62ebc69e0c9f58d657dd2003
Ramdhani <dhani.rachmansyah@georgebrown.ca>
2022-02-04 15:20:50 -0500

    token: add sol docs

--- contracts/LandToken.sol
+++ contracts/LandToken.sol
@@ -2,19 +2,17 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Greeter is ERC721 {
+/// @title LandToken
+/// @author Ramdhani Rachmansyah
+/// @notice Simple Land NFT Token
+/// @dev Simple Land NFT Token
+contract LandToken is ERC721 {
+    /// @notice Create new Land token
+    /// @dev Constructor to initialize new token and give the supplies to the owner
+    /// @param _totalSupply (uint256)
     constructor(uint256 _totalSupply) ERC721("LandToken", "LAND") {
         _mint(msg.sender, _totalSupply);
     }
-
-    // function greet() public view returns (string memory) {
-    //     return greeting;
-    // }
-
-    // function setGreeting(string memory _greeting) public {
-    //     console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-    //     greeting = _greeting;
-    // }
 }
